9c2406d6b4756dff3410f9592d07de33a6229a17
maja-melinova <56113759+maja-melinova@users.noreply.github.com>
2026-08-22 15:20:40 +0200

    Update main.py

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -2,6 +2,9 @@ import requests
 import os
 import zipfile
 import io
+from concurrent.futures import ThreadPoolExecutor
+import asyncio
+import aiohttp
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -26,11 +29,51 @@ def main():
             name = name.removesuffix('.zip')
 
             z = zipfile.ZipFile(io.BytesIO(r.content))
-            #z.extractall("downloads")
-            z.extract(name+".csv", "downloads")
+            #print(z.namelist())
+            z.extract(name + ".csv", "downloads")
+
+    # ------------------------------------------------------------
+    # aiohttp
+    # ------------------------------------------------------------
+    if not os.path.exists("downloads/aiohttp"):
+        os.makedirs("downloads/aiohttp")
+
+    async def download_aiohttp(uri):
+        r = requests.get(uri)
+
+        if r.ok:
+            name = os.path.split(uri)[1]
+            name = name.removesuffix('.zip')
+
+            z = zipfile.ZipFile(io.BytesIO(r.content))
+            z.extract(name + ".csv", "downloads/aiohttp")
+
+    async def fce():
+        tasks = [download_aiohttp(uri) for uri in download_uris]
+        await asyncio.gather(*tasks)
+
+    asyncio.run(fce())
+
+    #------------------------------------------------------------
+    #ThreadPoolExecutor
+    #------------------------------------------------------------
+    if not os.path.exists("downloads/ThreadPoolExecutor"):
+        os.makedirs("downloads/ThreadPoolExecutor")
+
+    def download_threadpool(uri):
+        r = requests.get(uri)
+
+        if r.ok:
+            name = os.path.split(uri)[1]
+            name = name.removesuffix('.zip')
+
+            z = zipfile.ZipFile(io.BytesIO(r.content))
+            z.extract(name + ".csv", "downloads/ThreadPoolExecutor")
+
+    with ThreadPoolExecutor() as executor:
+        executor.map(download_threadpool, download_uris)
 
     pass
-
 
 if __name__ == "__main__":
     main()
